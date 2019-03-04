3e3040b6b9e8f3d270711b8d39cf2e6eddb3523e
dreazz <agustinsilveira.96@gmail.com>
2019-03-04 15:21:34 +0100

    allow cross origin test

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ const app = express();
 
 app.use(cors({
   credentials: true,
-  origin: [process.env.PUBLIC_DOMAIN]
+  origin: [process.env.PUBLIC_DOMAIN],
+
 }));
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
